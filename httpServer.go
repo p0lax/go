18d12cf491ef6162c5e0fb1d29a581e3edfff988
Andrey <andrey.r@ati.su>
2015-10-09 15:48:19 +0300

    POST request handler

--- httpServer.go
+++ httpServer.go
@@ -1,30 +1,35 @@
 package main
 
 import (
+	"io/ioutil"
 	"encoding/json"
 	"net/http"
 	"log"
+	"fmt"
 )
 
 func handle(w http.ResponseWriter, r *http.Request) {
-	testMap := make(map[string]string)
+	paramsMap := make(map[string]string)
 	switch r.Method {
 	case "GET":
 		query := r.URL.Query()
 		for key := range query {
-			testMap[key] = string(query.Get(key))
+			paramsMap[key] = string(query.Get(key))
 		}
 		w.Header().Set("Content-Type", "application/json")
-		js, err := json.Marshal(testMap)
+		js, err := json.Marshal(paramsMap)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
 		w.Write(js)
-		// Serve the resource.
 	case "POST":
-		decoder := json.NewDecoder(r.Body)
+		hah, err := ioutil.ReadAll(r.Body);
+		if err != nil {
+    		fmt.Println(err)
+    	}
 		w.Header().Set("Content-Type", "application/json")
+		w.Write(hah)
 	// Create a new record.
 	case "PUT":
 	// Update an existing record.
